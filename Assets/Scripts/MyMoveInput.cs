13819972ae1b39f815c53c55cf4639ae001d6535
Beadhangerone <denm.2206@gmail.com>
2022-05-02 10:54:57 +0200

    Move and run

--- Assets/Scripts/MyMoveInput.cs
+++ Assets/Scripts/MyMoveInput.cs
@@ -11,91 +11,97 @@ public class MyMoveInput : MonoBehaviour
     public Camera mainCamera;
     private Transform _mainCameraTransform;
 
-    private float _currentVelocity;
+    private float _horizontalVelocity;
+    private float _verticalVelocity;
     private float _inputX, _inputY;
+
+    private float gravity = -9.8f;
+    private float groundedGravity = -.5f;
+    
     private Vector3 _desiredMoveDirection;
 
     private bool _isRunning = false;
+    private bool _isJumping = false;
 
     public float desiredVelocity = 10;
-    public float desiredRotationSpeed = 0.1f; 
-    
-    [Range(0,1f)]
-    public float startAnimDamp = 0.3f;
+    public float desiredRotationSpeed = 0.1f;
 
+    [Range(0, 1f)] public float startAnimDamp = 0.3f;
+
+    void handleGravity()
+    {
+        if (_characterController.isGrounded)
+        {
+            
+        }
+    }
+
+    //---move---
     private PlayerInput _playerInput;
-    private InputAction _playerInputAction;
+    private InputAction _playerInputActionMove;
+    private InputAction _playerInputActionRun;
+    private InputAction _playerInputActionJump;
 
     //---other---
     private static readonly int PlayerVelocity = Animator.StringToHash("playerVelocity");
+    private static readonly int PlayerVerticalVelocity = Animator.StringToHash("playerVerticalVelocity");
 
-    void Awake()
+    private void Awake()
     {
+        _animator = GetComponent<Animator>();
         _playerInput = gameObject.GetComponent<PlayerInput>();
-        _playerInputAction = _playerInput.actions["Run"];
+        _playerInputActionRun = _playerInput.actions["Run"];
+        _playerInputActionMove = _playerInput.actions["Move"];
+        _playerInputActionJump = _playerInput.actions["Jump"];
 
-        _playerInputAction.started += context =>
-        {
-            _isRunning = context.ReadValueAsButton();
-        };
-        
-        _playerInputAction.canceled += context =>
-        {
-            _isRunning = context.ReadValueAsButton();
-            _inputX /= 2;
-        };
+        _playerInputActionMove.started += context => MyOnMove(context);
+        _playerInputActionMove.performed += context => MyOnMove(context);
+        _playerInputActionMove.canceled += context => MyOnMove(context);
 
+        _playerInputActionRun.started += context => MyOnRun(context);
+        _playerInputActionRun.canceled += context => MyOnRun(context);
+
+        _playerInputActionJump.started += context => MyOnJump(context);
+        _playerInputActionJump.canceled += context => MyOnJump(context);
     }
-    //
-    // private void OnEnable()
-    // {
-    //     _playerInputAction.Enable();
-    // }
-    //
-    // private void OnDisable()
-    // {
-    //     _playerInputAction.Disable();
-    // }
-
-
-    // ---START---
+    
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
-        _animator = GetComponent<Animator> ();
         _mainCameraTransform = mainCamera.transform;
     }
-
-    
     void Update()
     {
-        if (_isRunning && _inputY + _inputX > 0 )
+        _horizontalVelocity = new Vector2(_inputX, _inputY).sqrMagnitude;
+
+        var forward = _mainCameraTransform.forward;
+        var right = _mainCameraTransform.right;
+        var up = _mainCameraTransform.up;
+            
+        // moving
+        forward.y = 0f;
+        right.y = 0f;
+        //falling
+        up.x = 0f;
+        up.z = 0f;
+        up.y = _verticalVelocity;
+
+        if (_horizontalVelocity == 0)
         {
-            _inputY = 1;
-            _inputY = 1;
-        }
-        _currentVelocity = new Vector2(_inputX, _inputY).sqrMagnitude;
-        
-        
-        if (_currentVelocity < 0.1)
-        {
-            _currentVelocity = 0;
             _animator.SetFloat (PlayerVelocity, 0.0f, 0, Time.deltaTime);
         }
-        else if (_currentVelocity > 0.1)
+        else if (_horizontalVelocity > 0.1 && _verticalVelocity == 0)
         {
-            var forward = _mainCameraTransform.forward;
-            var right = _mainCameraTransform.right;
-
-            forward.y = 0f;
-            right.y = 0f;
-
             forward.Normalize();
             right.Normalize();
-
+            up.Normalize();
+            
             _desiredMoveDirection = forward * _inputY + right * _inputX;
-            _animator.SetFloat(PlayerVelocity, _currentVelocity, startAnimDamp, Time.deltaTime);
+            _animator.SetFloat(PlayerVelocity, _horizontalVelocity, startAnimDamp, Time.deltaTime);
             _characterController.Move(_desiredMoveDirection.normalized * (Time.deltaTime * desiredVelocity));
+        }else if (_verticalVelocity != 0)
+        {
+            _animator.SetFloat (PlayerVerticalVelocity, _verticalVelocity, startAnimDamp, Time.deltaTime);
         }
 
 
@@ -104,15 +110,28 @@ public class MyMoveInput : MonoBehaviour
             transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (_desiredMoveDirection), desiredRotationSpeed);
         }
     }
-    
-    private void OnMove(InputValue movement)
+
+    private void MyOnMove(InputAction.CallbackContext context)
     {
-        Vector2 movementVect = movement.Get<Vector2>().normalized;
-
-        _inputX = movementVect.x / 2;
-        _inputY = movementVect.y / 2;
-
+        var movement = context.ReadValue<Vector2>().normalized;
+        
+        _inputX = movement.x / 2;
+        _inputY = movement.y / 2;
     }
-    
 
+    private void MyOnRun(InputAction.CallbackContext context)
+    {
+        _isRunning = context.ReadValueAsButton();
+        Debug.Log(_isRunning);
+        if (_isRunning) {
+            _inputY *= 2;
+        } else {
+            _inputY = 0.5f;
+        }
+    }
+
+    private void MyOnJump(InputAction.CallbackContext context)
+    {
+        _isJumping = context.ReadValueAsButton();
+    }
 }

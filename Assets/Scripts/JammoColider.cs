c23ae23779c3cf309cca633cf506ec632f128e2d
Dan Moskalchuck <denm.2206@gmail.com>
2022-05-14 21:16:37 +0200

    Death functionality

--- Assets/Scripts/JammoColider.cs
+++ Assets/Scripts/JammoColider.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class JammoColider : MonoBehaviour
 {
     public GameObject levelManager;
-    
+    public GameObject deadZone;
     private const string TrampolineTag = "Trampoline";
     private const string FinishTag = "FinishArch";
+    private const string DeadTag = "DeadZone";
     
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,13 @@ public class JammoColider : MonoBehaviour
         {
             levelManager.GetComponent<MyLevelManager>().FinishLevel1();
         }
+        
+        if (other.CompareTag(DeadTag))
+        {
+            levelManager.GetComponent<MyLevelManager>().FailLevel1();
+        }
+        deadZone.transform.position = new Vector3(transform.position.x, other.transform.position.y - 3, transform.position.z);
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/MyLevelManager.cs
+++ Assets/Scripts/MyLevelManager.cs
@@ -8,10 +8,12 @@ public class MyLevelManager : MonoBehaviour
 {
     private SaveManager _saveManager;
     public GameObject stopwatchManager;
+    private Stopwatch stopwatch;    
 
     private void Start()
     {
         _saveManager = SaveManager.Instance;
+        stopwatch = stopwatchManager.GetComponent<Stopwatch>();
     }
 
     public static void LoadLevel1()
@@ -21,9 +23,14 @@ public class MyLevelManager : MonoBehaviour
 
     public void FinishLevel1()
     {
-        Stopwatch stopwatch = stopwatchManager.GetComponent<Stopwatch>();
         stopwatch.Pause();
         _saveManager.PlayerSave.SaveResultForLevel(1, stopwatch.Time);
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void FailLevel1()
+    {
+        stopwatch.Reset();
+        SceneManager.LoadScene("Level1");
     }
 }

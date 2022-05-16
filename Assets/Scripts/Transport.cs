69852f762c08d2ad97f7914a4ac7d22c8a132fd4
Dan Moskalchuck <denm.2206@gmail.com>
2022-05-16 14:41:27 +0200

    some fixes of the moving platform,. still not moving the player

--- Assets/Scripts/Transport.cs
+++ Assets/Scripts/Transport.cs
@@ -14,6 +14,7 @@ public class Transport : MonoBehaviour
     
     public int movementDistance = 10;
     public char movementAxis = 'z';
+    public float movementSpeed;
 
     private Func<Vector3, bool> _finalPositionReached;
     private Func<Vector3, bool> _initialPositionReached;
@@ -21,6 +22,8 @@ public class Transport : MonoBehaviour
     void Start()
     {
         _initialPosition = gameObject.transform.position;
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        
         if (movementAxis == 'x')
         {
             _finalPosition = new Vector3(_initialPosition.x + movementDistance, _initialPosition.y, _initialPosition.z);   
@@ -28,28 +31,27 @@ public class Transport : MonoBehaviour
             _backMovementVector = new Vector3(-1, 0, 0);
             _finalPositionReached = (Vector3 currentPosition) => currentPosition.x >= _finalPosition.x;
             _initialPositionReached = (Vector3 currentPosition) => currentPosition.x <= _initialPosition.x;
+            _rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
         }else if (movementAxis == 'y')
         {
             _finalPosition = new Vector3(_initialPosition.x, _initialPosition.y + movementDistance, _initialPosition.z);
             _forthMovementVector = new Vector3(0, 1, 0);
             _backMovementVector = new Vector3(0, -1, 0);
-            _finalPositionReached = (Vector3 currentPosition) => currentPosition.y >= _finalPosition.x;
+            _finalPositionReached = (Vector3 currentPosition) => currentPosition.y >= _finalPosition.y;
             _initialPositionReached = (Vector3 currentPosition) => currentPosition.y <= _initialPosition.y;
-
+            _rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
         }
         else
         {
             _finalPosition = new Vector3(_initialPosition.x, _initialPosition.y, _initialPosition.z + movementDistance);
-            _forthMovementVector = new Vector3(0, 0, 1);
-            _backMovementVector = new Vector3(0, 0, -1);
-            _finalPositionReached = (Vector3 currentPosition) => currentPosition.z >= _finalPosition.x;
+            _forthMovementVector = new Vector3(0, 0, movementSpeed);
+            _backMovementVector = new Vector3(0, 0, -movementSpeed);
+            _finalPositionReached = (Vector3 currentPosition) => currentPosition.z >= _finalPosition.z;
             _initialPositionReached = (Vector3 currentPosition) => currentPosition.z <= _initialPosition.z;
+            _rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
 
         }
         
-        //Debug.Log(_initialPosition +" -> "+ _finalPosition);
-        
-        _rigidbody = gameObject.GetComponent<Rigidbody>();
         _currentMovementVector = _forthMovementVector;
         Debug.Log(_initialPosition+"->"+_finalPosition);
     }
@@ -58,8 +60,6 @@ public class Transport : MonoBehaviour
     public void Update()
     {
         Vector3 currentPosition = gameObject.transform.position;
-
-        Debug.Log(currentPosition+": "+_initialPositionReached(currentPosition)+"->"+_finalPositionReached(currentPosition));
         
         if (_finalPositionReached(currentPosition))
         {
@@ -69,8 +69,8 @@ public class Transport : MonoBehaviour
         {
             _currentMovementVector = _forthMovementVector;
         }
-        
-        _rigidbody.AddForce(_currentMovementVector);
+        Debug.Log(currentPosition+": "+_currentMovementVector);
+        gameObject.transform.position = currentPosition + _currentMovementVector * Time.deltaTime;
         
     }
     

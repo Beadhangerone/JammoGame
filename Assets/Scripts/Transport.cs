40789fe9419d707096e3b90f34d276b5028395bb
Dan Moskalchuck <denm.2206@gmail.com>
2022-05-16 20:04:28 +0200

    Negative movement distance implemented

--- Assets/Scripts/Transport.cs
+++ Assets/Scripts/Transport.cs
@@ -14,7 +14,7 @@ public class Transport : MonoBehaviour
     private bool _isPlayerOnMe;
     
     public GameObject player;
-    private MyMoveInput _moveInput; 
+    private MyMoveInput _playerMove; 
 
     public int movementDistance = 10;
     public char movementAxis = 'z';
@@ -22,27 +22,57 @@ public class Transport : MonoBehaviour
 
     private Func<Vector3, bool> _finalPositionReached;
     private Func<Vector3, bool> _initialPositionReached;
+
+    private bool CompareFinalPos(float current, float final)
+    {
+        if (movementDistance > 0)
+        {
+            return current >= final;
+        }
+        else
+        {
+            return current <= final;
+        }
+    }
+
+    private bool CompareInitialPos(float current, float initial)
+    {
+        if (movementDistance > 0)
+        {
+            return current <= initial;
+        }
+        else
+        {
+            return current >= initial;
+        }
+    }
+
     void Awake()
     {
+        if (movementDistance < 0)
+        {
+            movementSpeed *= -1;
+        }
+        
         _initialPosition = gameObject.transform.position;
         _rigidbody = gameObject.GetComponent<Rigidbody>();
-        _moveInput = player.GetComponent<MyMoveInput>();
+        _playerMove = player.GetComponent<MyMoveInput>();
         
         if (movementAxis == 'x')
         {
             _finalPosition = new Vector3(_initialPosition.x + movementDistance, _initialPosition.y, _initialPosition.z);   
             _forthMovementVector = new Vector3(movementSpeed, 0, 0);
             _backMovementVector = new Vector3(-movementSpeed, 0, 0);
-            _finalPositionReached = (Vector3 currentPosition) => currentPosition.x >= _finalPosition.x;
-            _initialPositionReached = (Vector3 currentPosition) => currentPosition.x <= _initialPosition.x;
+            _finalPositionReached = (Vector3 currentPosition) => CompareFinalPos(currentPosition.x , _finalPosition.x);
+            _initialPositionReached = (Vector3 currentPosition) => CompareInitialPos(currentPosition.x, _initialPosition.x);
             _rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
         }else if (movementAxis == 'y')
         {
             _finalPosition = new Vector3(_initialPosition.x, _initialPosition.y + movementDistance, _initialPosition.z);
             _forthMovementVector = new Vector3(0, movementSpeed, 0);
             _backMovementVector = new Vector3(0, -movementSpeed, 0);
-            _finalPositionReached = (Vector3 currentPosition) => currentPosition.y >= _finalPosition.y;
-            _initialPositionReached = (Vector3 currentPosition) => currentPosition.y <= _initialPosition.y;
+            _finalPositionReached = (Vector3 currentPosition) => CompareFinalPos(currentPosition.y , _finalPosition.y);
+            _initialPositionReached = (Vector3 currentPosition) => CompareInitialPos(currentPosition.y, _initialPosition.y);
             _rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
         }
         else
@@ -50,12 +80,13 @@ public class Transport : MonoBehaviour
             _finalPosition = new Vector3(_initialPosition.x, _initialPosition.y, _initialPosition.z + movementDistance);
             _forthMovementVector = new Vector3(0, 0, movementSpeed);
             _backMovementVector = new Vector3(0, 0, -movementSpeed);
-            _finalPositionReached = (Vector3 currentPosition) => currentPosition.z >= _finalPosition.z;
-            _initialPositionReached = (Vector3 currentPosition) => currentPosition.z <= _initialPosition.z;
+            _finalPositionReached = (Vector3 currentPosition) => CompareFinalPos(currentPosition.z , _finalPosition.z);
+            _initialPositionReached = (Vector3 currentPosition) => CompareInitialPos(currentPosition.z, _initialPosition.z);
             _rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
         }
         
         _currentMovementVector = _forthMovementVector;
+
     }
 
     // Update is called once per frame
@@ -69,7 +100,7 @@ public class Transport : MonoBehaviour
             _currentMovementVector = _backMovementVector;
             if (_isPlayerOnMe)
             {
-                _moveInput.externalMovement = _currentMovementVector;
+                _playerMove.externalMovement = _currentMovementVector;
             }
         }
         else if(_initialPositionReached(currentPosition))
@@ -77,7 +108,7 @@ public class Transport : MonoBehaviour
             _currentMovementVector = _forthMovementVector;
             if (_isPlayerOnMe)
             {
-                _moveInput.externalMovement = _currentMovementVector;
+                _playerMove.externalMovement = _currentMovementVector;
             }
         }
         gameObject.transform.Translate( _currentMovementVector * Time.deltaTime);
@@ -89,7 +120,7 @@ public class Transport : MonoBehaviour
         if (other == player)
         {
             _isPlayerOnMe = true;
-            _moveInput.externalMovement = _currentMovementVector;
+            _playerMove.externalMovement = _currentMovementVector;
         }
     }
     
@@ -99,7 +130,7 @@ public class Transport : MonoBehaviour
         if (other == player)
         {
             _isPlayerOnMe = false;
-            _moveInput.externalMovement = Vector3.zero;
+            _playerMove.externalMovement = Vector3.zero;
         }
     }
 }
